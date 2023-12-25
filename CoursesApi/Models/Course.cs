3b0b2a038fed07ae31a29ba3fd96f706d9bdee14
Diego Chavez <diegochavezcarro@gmail.com>
2023-12-25 10:26:11 -0300

    Name could be nullable

--- CoursesApi/Models/Course.cs
+++ CoursesApi/Models/Course.cs
@@ -3,6 +3,6 @@ namespace CoursesApi.Models
     public class Course
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
